a04d19f9e5303a812158533e3341f36248452c84
PaulUJS <p.ulerick17@gmail.com>
2023-03-15 17:16:52 -0400

    implemented basic parsing and expression evaluation

--- interpreter.py
+++ interpreter.py
@@ -8,6 +8,56 @@ Env = dict
 
 tokens = []
 
+class Expression:
+    op = ''
+    index = 0
+    expr = []
+    num = []
+    result = None
+
+    def listify(self):
+        count = 0
+        ops = ['Plus', 'Minus', 'Star', 'Slash']
+        if len(self.expr) == 0:
+            self.expr.append([])
+        for i in tokens:
+            count += 1
+            if i in ops:
+                self.expr[self.index].append(i)
+            elif num_match(i):
+                self.expr[self.index].append(i)
+            elif i == 'LeftParen' and count > 1:
+                self.expr.append([])
+                self.index += 1
+        print(self.expr)
+        self.evaluate()
+
+    def evaluate(self):
+        index = len(self.expr) - 1
+        ops = {
+            'Plus': lambda x, y: x + y,
+            'Minus': lambda x, y: x - y,
+            'Star': lambda x, y: x * y,
+            'Slash': lambda x, y: x // y,
+          }
+        res = []
+        while index >= 1:
+            for i in self.expr[index]:
+                if ops.get(i):
+                    self.op = i
+                if num_match(i):
+                    self.num.append(int(i))
+            res.append(ops[self.op](self.num[0], self.num[1]))
+            self.num.clear()
+            index -= 1
+        for v in self.expr[index]:
+            if ops.get(v):
+                self.op = v
+        print(res)
+        print(ops[self.op](res[0], res[1]))
+        
+
+
 def read_contents() -> str:
     prog_file = open("test.txt", "r")
     res = prog_file.readlines()
@@ -44,26 +94,6 @@ def add_token(token: str) -> list:
     tokens.append(token)
     return tokens
 
-def evaluate():
-    ops = {
-            'Plus': lambda x, y: print(x + y),
-            'Minus': lambda x, y: print(x - y),
-            'Star': lambda x, y: print(x * y),
-            'Slash': lambda x, y: print(x / y)
-          }
-    op = ''
-    num = []
-    for i in tokens:
-        if len(num) != 2:
-            if ops.get(i):
-                op = i
-            if num_match(i):
-                num.append(int(i))
-        else:  
-            ops[op](num[0], num[1])
-            num.clear()
-            op = ''
-
-
 read_contents()
-evaluate()
+exp = Expression()
+exp.listify()

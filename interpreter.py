870730b3392a0fd4c5c1a80cc03488c5497ec8ee
PaulUJS <p.ulerick17@gmail.com>
2023-03-15 22:16:23 -0400

    worked on parsing and tokenizing, interpreter can now identify each expression one by one

--- interpreter.py
+++ interpreter.py
@@ -1,21 +1,14 @@
-# Type definitions for the interpreter
-Symbol = str
-Number = (int, float)
-Atom = (Symbol, Number)
-List = list
-Exp = (Atom, List)
-Env = dict
-
-tokens = []
-
 class Expression:
-    op = ''
-    index = 0
-    expr = []
-    num = []
-    result = None
+    def __init__(self, op, expr, num, index):
+        self.op = op
+        self.expr = expr
+        self.num = num
+        self.index = index
 
-    def listify(self):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def listify(self, tokens):
         count = 0
         ops = ['Plus', 'Minus', 'Star', 'Slash']
         if len(self.expr) == 0:
@@ -24,7 +17,7 @@ class Expression:
             count += 1
             if i in ops:
                 self.expr[self.index].append(i)
-            elif num_match(i):
+            elif lispy.num_match(i):
                 self.expr[self.index].append(i)
             elif i == 'LeftParen' and count > 1:
                 self.expr.append([])
@@ -45,7 +38,7 @@ class Expression:
             for i in self.expr[index]:
                 if ops.get(i):
                     self.op = i
-                if num_match(i):
+                if lispy.num_match(i):
                     self.num.append(int(i))
             res.append(ops[self.op](self.num[0], self.num[1]))
             self.num.clear()
@@ -55,45 +48,52 @@ class Expression:
                 self.op = v
         print(res)
         print(ops[self.op](res[0], res[1]))
-        
 
+class interpreter:
+    tokens = []
+    def read_contents(self) -> str:
+        prog_file = open("test.txt", "r")
+        res = prog_file.readlines()
+        listToStr = ' '.join([str(elem) for elem in res])
+        self.tokenize(listToStr)
 
-def read_contents() -> str:
-    prog_file = open("test.txt", "r")
-    res = prog_file.readlines()
-    listToStr = ' '.join([str(elem) for elem in res])
-    tokenize(listToStr)
+    def num_match(self, num: str) -> bool:
+        nums = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+        if nums.get(num):
+            return True
+        return False
 
-def num_match(num: str) -> bool:
-    nums = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-    if nums.get(num):
-        return True
-    return False
+    def op_match(self, op: str) -> str:
+        ops = {'+': 'Plus', '-': 'Minus', '*': 'Star', '/': 'Slash'}
+        if ops.get(op):
+            #add_token(ops.get(op))
+            return ops.get(op)
 
-def op_match(op: str) -> str:
-    ops = {'+': 'Plus', '-': 'Minus', '*': 'Star', '/': 'Slash'}
-    if ops.get(op):
-        #add_token(ops.get(op))
-        return ops.get(op)
+    def tokenize(self, chars: str) -> list:
+        print(chars)
+        left = 0
+        right = 0
+        for c in chars:
+            if left != right or left == 0 :
+                if c == '(':
+                    left += 1
+                    self.add_token('LeftParen')
+                elif c ==')':
+                    right += 1
+                    self.add_token('RighParen')
+                elif self.num_match(c):
+                    self.add_token(c)
+                elif self.op_match(c): 
+                    self.add_token(self.op_match(c))
+            elif left == right and left > 0:
+                left = 0; right = 0;
+                object = Expression('', [], [], 0)
+                object.listify(self.tokens)
+                self.tokens = []
 
-def tokenize(chars: str) -> list:
-    #chars.replace('(', ' ( ').replace(')', ' ) ').split()
-    print(chars)
-    for c in chars:
-        if c == '(':
-            add_token('LeftParen')
-        elif c ==')':
-            add_token('RighParen')
-        elif num_match(c):
-            add_token(c)
-        elif op_match(c): 
-            add_token(op_match(c))
-    print(tokens)
+    def add_token(self, token: str) -> list:
+        self.tokens.append(token)
+        return self.tokens
 
-def add_token(token: str) -> list:
-    tokens.append(token)
-    return tokens
-
-read_contents()
-exp = Expression()
-exp.listify()
+lispy = interpreter()
+lispy.read_contents()
